86e29f34807baa106fc262edd9e47563c3804d81
HongHyukJin <gurwlszx@naver.com>
2020-02-27 16:08:00 +0900

    0227

--- Pickme/urls.py
+++ Pickme/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('edit.html', views.edit, name='edit')
+    path('edit.html', views.edit, name='edit'),
+    path('base.html', views.base, name='base'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, documnet_root=settings.MEDIA_ROOT)
--- Pickme/views.py
+++ Pickme/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
-
+from django.views.generic import TemplateView
+from django.core.files.storage import FileSystemStorage
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/pickme.html', {})
+    return render(request, 'blog/Pickme.html', {})
 
 def edit(request):
+    if request.method =='POST':
+        uploaded_file = request.FILES['document']
+        print(uploaded_file.name)
+        print(uploaded_file.size)
+        fs = FileSystemStorage()
+        name = fs.save(uploaded_file.name, uploaded_file)
+        url = fs.url(name)
+        print(url)
     return render(request, 'blog/edit.html', {})
+
+def base(request):
+    return render(request, 'blog/base.html', {})
